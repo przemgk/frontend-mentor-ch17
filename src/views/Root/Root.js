b142f8d54d9470c137ca0d0b96306c436d4e41c7
przemgk <przem_gk@o2.pl>
2020-01-09 19:29:41 +0100

    Add global styles and install sass

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-const Root = () => <BrowserRouter />;
+const Root = () => (
+  <BrowserRouter>
+    <h1>Site under construction</h1>
+  </BrowserRouter>
+);
 
 export default Root;
