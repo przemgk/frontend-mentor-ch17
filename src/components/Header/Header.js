3b2d56185a5bb6a028d9c0c6afc51291a5fabe06
przemgk <przem_gk@o2.pl>
2020-01-09 20:18:42 +0100

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from 'assets/logo.svg';
+import styles from 'components/Header/Header.module.scss';
+
+const Header = () => (
+  <div className={styles.header}>
+    <Link to="/">
+      <img className={styles.logo} src={Logo} alt="Rock, paper, scissors, lizard and spock" />
+    </Link>
+    <div className={styles.score}>
+      <span className={styles.score__label}>Score</span>
+      <span className={styles.score__points}>12</span>
+    </div>
+  </div>
+);
+
+export default Header;
