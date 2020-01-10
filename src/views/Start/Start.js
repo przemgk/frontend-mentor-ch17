b55e663515adac210f3bd4c31e2d3c0461ad528c
przemgk <przem_gk@o2.pl>
2020-01-10 01:11:35 +0100

    Add Start view without positioning

--- src/views/Start/Start.js
+++ src/views/Start/Start.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import styles from 'views/Start/Start.module.scss';
+import Roundel from 'components/Roundel';
 
-const Start = () => <p>ass</p>;
+const Start = () => (
+  <div className={styles.wrapper}>
+    <Roundel roundelType="Scissors" />
+    <Roundel roundelType="Lizard" />
+    <Roundel roundelType="Paper" />
+    <Roundel roundelType="Rock" />
+    <Roundel roundelType="Spock" />
+  </div>
+);
 
 export default Start;
